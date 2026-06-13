dfc00ee7bef80a60b4c6b29dde7f78c3eed7f55e
shauryaverma03 <shauryaverma036@gmail.com>
2026-06-14 03:11:47 +0530

    fix: CORS whitelist with CLIENT_URL env var

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,14 @@ const balanceRoutes = require('./routes/balance.routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    process.env.CLIENT_URL,
+  ].filter(Boolean),
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
